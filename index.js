78c94bb7d8eb9b44781c203cc6fe4a8528d134f7
Abraham851 <smishu851@gmail.com>
2022-05-08 17:20:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb')
+const port = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edazl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('ceramic').collection('product');
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        })
+
+    }
+    finally { }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+
+    res.send('site is raning')
+
+});
+
+app.listen(port, () => {
+    console.log('Site is ranning on port');
+})
