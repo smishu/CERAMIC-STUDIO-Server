0c2724a946ea4cada843a586cad8cd36cbea2eea
Abraham851 <smishu851@gmail.com>
2022-05-09 21:21:47 +0600

    find one product

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
@@ -16,18 +16,19 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('ceramic').collection('product');
-        app.get('/product', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = productCollection.find(query);
             const products = await cursor.toArray();
             res.send(products);
 
         });
-        app.get('/product/:id', async (req, res) => {
-            const id = req.params.id;
+        app.get('/product', async (req, res) => {
+            const id = req.query.id
+            console.log(id);
             const query = { _id: ObjectId(id) };
             const service = await productCollection.findOne(query);
-            res.send(product);
+            res.send(service);
         });
 
     }
