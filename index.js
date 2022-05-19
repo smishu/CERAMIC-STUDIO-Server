874c44fb1cd484f03494a0bc614a809947539462
Abraham851 <smishu851@gmail.com>
2022-05-19 18:41:16 +0600

    Testing now

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 
-;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.edazl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -86,7 +86,7 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
 
-    res.send('site is raning')
+    res.send('site is raning now')
 
 });
 
