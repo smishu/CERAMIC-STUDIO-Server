aee9b5da505366ca4847c5ced5994762916b99a0
Abraham851 <smishu851@gmail.com>
2022-05-19 18:52:38 +0600

    deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 const app = express();
 app.use(cors());
 app.use(express.json());
