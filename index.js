48582083558f966b4302e51dd91926f23e0e6606
Abraham851 <smishu851@gmail.com>
2022-05-19 19:08:17 +0600

    commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
-const port = process.env.PORT || 6000;
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -86,7 +86,7 @@ run().catch(console.dir);
 
 app.get('/', (req, res) => {
 
-    res.send('site is raning now')
+    res.send('site is ranning')
 
 });
 
